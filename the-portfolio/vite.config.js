f48b59aa9dec3933b4e3a635ea624733fabb5298
Jaclyn Pham <pham02@allegheny.edu>
2025-05-05 08:26:40 -0400

    feat: dynamic adjust chunk size

--- the-portfolio/vite.config.js
+++ the-portfolio/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'three-vendor': ['three', '@react-three/fiber', '@react-three/drei'],
+          'spline-vendor': ['@splinetool/react-spline', '@splinetool/runtime'],
+          'ui-vendor': ['lucide-react', 'framer-motion'],
+          'physics-vendor': ['matter-js'],
+        },
+      },
+    },
+  },
 })
